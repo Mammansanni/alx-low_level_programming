e41763dba3b8b61f59c3a6f6b141770466a90bc1
Mammansanni <umarmammansanni@gmail.com>
2023-02-21 15:12:41 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - check for absolute value
+ *
+ * @a: parameter to be checkd
+ *
+ * Return: always a
+ */
+
+int _abs(int)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
