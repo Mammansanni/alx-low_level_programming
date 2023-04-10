b8a3283bf0b930c31c682d36b92ec05257f06041
Mammansanni <umarmammansanni@gmail.com>
2023-04-10 09:13:00 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,24 +11,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int ams, w, length = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
-			length++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	ams = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(ams, text_content, length);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if (ams == -1 || w == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	close(ams);
+	close(fd);
 
 	return (1);
 }
